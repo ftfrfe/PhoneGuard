9ab663367e70cf02385893ac433f44333dc7367f
ggyy <ftfrfefy@Gmail.com>
2016-11-23 11:30:23 +0800

    0.2.1 防丢失功能 布局

--- app/src/main/java/com/example/ggyy/phoneguard/activity/HomeActivity.java
+++ app/src/main/java/com/example/ggyy/phoneguard/activity/HomeActivity.java
@@ -93,7 +93,40 @@ public class HomeActivity extends Activity {
     }
 
     private void showPasswordInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final AlertDialog dialog = builder.create();
+        View view = View.inflate(this, R.layout.dailog_input_password, null);
+        dialog.setView(view, 0, 0, 0, 0);
+        final EditText etPassword = (EditText) view.findViewById(R.id.et_password);
+        Button btnOK = (Button) view.findViewById(R.id.btn_ok);
+        Button btnCancel = (Button) view.findViewById(R.id.btn_cancel);
 
+        btnOK.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String password = etPassword.getText().toString();
+                if (!TextUtils.isEmpty(password)) {
+                    String savedPassword = sharedPreferences.getString("password", null);
+
+                    if (MD5Utils.encode(password).equals(savedPassword)) {
+                        dialog.dismiss();
+                        startActivity(new Intent(HomeActivity.this,
+                                LostFindActivity.class));
+                    } else {
+                        ToastUtils.showToast(act, "密码输出错误");
+                    }
+                } else {
+                    ToastUtils.showToast(act, "密码输入不能为空");
+                }
+            }
+        });
+        btnCancel.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();// 隐藏dialog
+            }
+        });
+        dialog.show();
     }
 
     private void showPasswordSetDialog() {
@@ -107,8 +140,8 @@ public class HomeActivity extends Activity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         final AlertDialog dialog = builder.create();
         View view = View.inflate(this, R.layout.dailog_set_password, null);
-        dialog.setView(view,0,0,0,0);
-        final EditText etPassword= (EditText) view.findViewById(R.id.et_password);
+        dialog.setView(view, 0, 0, 0, 0);
+        final EditText etPassword = (EditText) view.findViewById(R.id.et_password);
         final EditText etPassworConfirm = (EditText) view.findViewById(R.id.et_password_confirm);
 
         Button btnOk = (Button) view.findViewById(R.id.btn_ok);
@@ -120,20 +153,20 @@ public class HomeActivity extends Activity {
                 String password = etPassword.getText().toString();
                 String passwordConfirm = etPassworConfirm.getText().toString();
 
-                if(!TextUtils.isEmpty(password) && !passwordConfirm.isEmpty()){
-                    if (password.equals(passwordConfirm)){
-                        sharedPreferences.edit().putString("password",MD5Utils.encode(password)).commit();
-                        ToastUtils.showToast(act,"密码提交成功");
+                if (!TextUtils.isEmpty(password) && !passwordConfirm.isEmpty()) {
+                    if (password.equals(passwordConfirm)) {
+                        sharedPreferences.edit().putString("password", MD5Utils.encode(password)).commit();
+                        ToastUtils.showToast(act, "密码提交成功");
                         dialog.dismiss();
-                    }
-                    else {
-                        ToastUtils.showToast(act,"两次密码不同");
+                        startActivity(new Intent(HomeActivity.this, LostFindActivity.class));
+                        System.out.println("跳转到方丢失页面");
+                    } else {
+                        ToastUtils.showToast(act, "两次密码不同");
                     }
 
 
-                    }
-                else {
-                    ToastUtils.showToast(act,"输入框内容不能为空!");
+                } else {
+                    ToastUtils.showToast(act, "输入框内容不能为空!");
                 }
             }
         });
@@ -164,7 +197,7 @@ public class HomeActivity extends Activity {
 
         @Override
         public View getView(int i, View contentView, ViewGroup viewGroup) {
-            View view = View.inflate(HomeActivity.this, R.layout.home_list_item,null);
+            View view = View.inflate(HomeActivity.this, R.layout.home_list_item, null);
             ImageView ivView = (ImageView) view.findViewById(R.id.iv_item);
             TextView tvView = (TextView) view.findViewById(R.id.tv_item);
 
---
+++ app/src/main/java/com/example/ggyy/phoneguard/activity/LostFindActivity.java
@@ -0,0 +1,15 @@
+package com.example.ggyy.phoneguard.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.ggyy.phoneguard.R;
+
+public class LostFindActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_find);
+    }
+}
