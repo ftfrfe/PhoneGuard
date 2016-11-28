2db8397966328a6e3839bf1cb10e9bca74b6a357
ggyy <ftfrfefy@Gmail.com>
2016-11-28 15:24:50 +0800

    0.2.7  号码归属地 dao

--- app/src/main/java/com/example/ggyy/phoneguard/service/AddressService.java
+++ app/src/main/java/com/example/ggyy/phoneguard/service/AddressService.java
@@ -5,9 +5,12 @@ import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.view.View;
 import android.view.WindowManager;
+
+import java.sql.SQLOutput;
 
 public class AddressService extends Service {
     private TelephonyManager tm;
@@ -23,16 +26,19 @@ public class AddressService extends Service {
     private int winWidth;
     private int winHeight;
 
+    public AddressService() {
+
+    }
+
     @SuppressLint("ServiceCast")
     @Override
     public void onCreate() {
         super.onCreate();
         mPref = getSharedPreferences("config", MODE_PRIVATE);
-       tm = (TelephonyManager) getSystemService(TELECOM_SERVICE);
+        tm = (TelephonyManager) getSystemService(TELECOM_SERVICE);
         System.out.println("号码获取服务开启了");
-    }
-
-    public AddressService() {
+        listener = new MyListener();
+        tm.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
 
     }
 
@@ -41,15 +47,24 @@ public class AddressService extends Service {
         return null;
     }
 
-    class MyListener {
-    }
-
-    private class OutCallReceiver {
-    }
-
     @Override
     public void onDestroy() {
         super.onDestroy();
         System.out.println("号码获取服务关闭了");
+    }
+
+    class MyListener extends PhoneStateListener {
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            switch(state){
+                case TelephonyManager.CALL_STATE_RINGING:
+                    System.out.println("电话铃响了........");
+
+            };
+            super.onCallStateChanged(state, incomingNumber);
+        }
+    }
+
+    private class OutCallReceiver {
     }
 }
